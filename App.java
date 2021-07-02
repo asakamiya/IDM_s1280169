beb26a8aeca5056f6cb3b1931176d75c5d5e92df
asakamiya <85790397+asakamiya@users.noreply.github.com>
2021-07-02 12:29:53 +0900

    Greetingsadded

--- App.java
+++ App.java
@@ -1,13 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Random ran = new Random();
+        Scanner scn= new Scanner(System.in);
         int dice1 = ran.nextInt(6) +1;
         int dice2 = ran.nextInt(6) +1;
-        System.out.println("Rolling the Dice...");
-        System.out.println("Dice 1: "+ dice1);
-        System.out.println("Dice 2: "+ dice2);
+
+        System.out.println("What is your name?");
+        String name = scn.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+        System.out.println("Rolling the dice...");
+        System.out.println("Die 1: "+ dice1);
+        System.out.println("Die 2: "+ dice2);
         System.out.println("Total value: "+ (dice1 + dice2));
     }
 }
